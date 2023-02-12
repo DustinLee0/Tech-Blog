d13b55fe5928379d5d1b1a5a51a33bc3ae5546ec
Dustin Lee <leedustin@live.com>
2023-02-12 17:52:07 -0500

    update post route added

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Posts, Comments, User } = require('../models')
-const { auth } = require('../utils/helpers');
 
 router.get('/', async (req,res)=>{
     try{
@@ -16,9 +15,9 @@ router.get('/', async (req,res)=>{
           const posts = dbPosts.map((post) =>
           post.get({ plain: true })
         );
-          console.log('SERIALIZED POSTS: ', posts);
-          console.log('loggedIn: ', req.session.loggedIn);
-          console.log('userid: ', req.session.user_id);
+          // console.log('SERIALIZED POSTS: ', posts);
+          // console.log('loggedIn: ', req.session.loggedIn);
+          // console.log('userid: ', req.session.user_id);
 
         res.render('homepage', {
           posts,
@@ -28,15 +27,5 @@ router.get('/', async (req,res)=>{
         res.status(500).json(err);
     }
 });
-
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await Posts.findByPk(req.params.id)
-
-
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
 
 module.exports = router;
