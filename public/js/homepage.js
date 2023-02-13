46d50596e64635f85fcb0144d581f7460a1b2b30
Dustin Lee <leedustin@live.com>
2023-02-13 16:03:35 -0500

    comment routes and handlebars added

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,14 +1,14 @@
 const readMoreBtn = document.querySelectorAll('#read-more');
 
+// redirect to single post page to view post and/or comment
 readMoreBtn.forEach(button => {
     button.addEventListener('click', async (e) => {
         let event = e.target;
-        console.log('button clicked');
-
         let postId = event.parentElement.getAttribute('id');
-        console.log(postId)
+        console.log(postId);
 
-
+        if (postId) {
+            document.location.replace(`/single-post/${postId}`);
+        }
     })
-
 });
