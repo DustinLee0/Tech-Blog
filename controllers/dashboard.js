d8ded09e78ea23ed160ac5e73ec948a48e777e3a
Dustin Lee <leedustin@live.com>
2023-02-13 17:48:38 -0500

    httpheader bug

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -27,7 +27,7 @@ router.get('/', async (req, res) => {
             });
         }
 
-        res.render('dashboard');
+        // res.render('dashboard');
     } catch (err) {
         res.status(500).json(err);
     }
